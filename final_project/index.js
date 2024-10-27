0bd32dcb80119e6b777cb5f102bc7315fa949955
theworstkhoi <kylekhoitv@gmail.com>
2024-10-28 01:42:00 +0700

    feat: basic authentication and authorization using JWT

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // check if the authorization header is provided
+    if (!req.session.authorization) 
+        res.status(403).json({ messaage: "User not logged in"});
+
+    const accessToken = req.session.authorization["accessToken"];
+    jwt.verify(accessToken, "my_secret", (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ err });
+        }
+
+        console.log(decoded);
+        next();
+    })
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,23 +3,68 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  {
+    username: "user1",
+    password: "password1"
+  },
+  {
+    username: "user2",
+    password: "password2"
+  },
+  {
+    username: "user3",
+    password: "password3"
+  },
+];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  const foundUser = users.find(user => user.username === username);
+  if (!foundUser)
+    return false;
+  return true;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  const foundUser = users.find(user => user.username === username);
+  if (!foundUser)
+    return false;
+
+  const correctPassword = foundUser.password === password;
+  if (!correctPassword)
+    return false;
+
+  return true;
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res.status(401).send("Invalid credentials");
+
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign({ username }, "my_secret", { expiresIn: 70 });
+
+    req.session.authorization = {
+      accessToken,
+      username
+    }
+
+    return res.status(200).send("Successfully logged in");
+  } else {
+    return res.status(401).send("Invalid credentials");
+  }
 });
 
-// Add a book review
+// Endpoint used to test the authorization process
+regd_users.get("/auth/test", (req, res) => {
+  res.status(200).send("Accessed");
+});
+
+  // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
